ff5aa0451e50908c86f551c56e81c9f8d457f728
WillNotFit <abhimanyuvashisht.av@gmail.com>
2017-12-17 03:52:00 +0530

    inheritence
    Signed-off-by: WillNotFit <abhimanyuvashisht.av@gmail.com>

--- myFirstContract.sol
+++ myFirstContract.sol
@@ -1,16 +1,61 @@
-pragma solidity ^0.4.0;
+pragma solidity ^0.4.6; 
 
-contract MyFirstContract {
-	string private name;
-	uint private age;
-	
-	function setName(string newName){
-		name = newName;	
-	}
-	
-	function getName() returns(string){
-		return name;	
-	}
-	
-
+interface Regulator{
+    function checkValue(uint amount) returns(bool);
+    function load() returns(bool);
 }
+
+contract Bank is Regulator{
+    uint private value;
+    
+    function Bank(uint amount){
+        value = amount;
+    }
+    
+    function deposit(uint amount){
+        value += amount;
+    }
+    
+    function withdraw(uint amount){
+        if(checkValue(amount)){
+            value -= amount;
+        }
+    }
+    
+    function balance() returns(uint){
+        return value;
+    }
+    
+    function checkValue(uint amount) returns(bool){
+        return value >= amount;
+    }
+    
+    function loan() returns(bool){
+        return value > 0;
+    }
+}
+
+
+
+contract MyFirstContract is Bank{
+    string private name;
+    uint private age;
+
+    function setName(string newName){
+        name = newName;
+    }
+    
+    function getName() returns(string){
+        return name;
+    }
+    
+    function setAge(uint newAge){
+        age = newAge;
+    }
+    
+    function getAge() returns(uint){
+        return age;
+    }
+}
+
+
